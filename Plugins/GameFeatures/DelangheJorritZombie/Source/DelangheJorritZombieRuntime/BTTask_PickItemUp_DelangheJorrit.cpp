86334c9f99bd31b3e1e4e3388944cd7f300d5e53
jorritDelanghe <157893404+jorritDelanghe@users.noreply.github.com>
2026-05-30 23:45:48 +0200

    items are being pickedup

---
+++ Plugins/GameFeatures/DelangheJorritZombie/Source/DelangheJorritZombieRuntime/BTTask_PickItemUp_DelangheJorrit.cpp
@@ -0,0 +1,35 @@
+﻿#include "BTTask_PickItemUp_DelangheJorrit.h"
+
+#include "AIController.h"
+#include "BehaviorTree/BlackboardComponent.h"
+#include "Common/InventoryComponent.h"
+#include "Items/BaseItem.h"
+
+UBTTask_PickItemUp_DelangheJorrit::UBTTask_PickItemUp_DelangheJorrit()
+{
+	NodeName="Grab item";
+}
+
+EBTNodeResult::Type UBTTask_PickItemUp_DelangheJorrit::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
+{
+	APawn* Pawn{OwnerComp.GetAIOwner()->GetPawn()};
+	if (!Pawn) return EBTNodeResult::Failed;
+	
+	ABaseItem* Item {Cast<ABaseItem>(
+		OwnerComp.GetBlackboardComponent()->GetValueAsObject(
+			TargetItemKey.SelectedKeyName))} ;
+	
+	if (!Item) return EBTNodeResult::Failed;
+	
+	auto* Inventory{Pawn->GetComponentByClass<UInventoryComponent>()};
+	if (!Inventory) return EBTNodeResult::Failed;
+	
+	for (int i{}; i<Inventory->GetInventoryCapacity();++i)
+	{
+		if (Inventory->GrabItem(i,Item))
+		{
+			return EBTNodeResult::Succeeded;
+		}
+	}
+	return EBTNodeResult::Failed;
+}
---
+++ Plugins/GameFeatures/DelangheJorritZombie/Source/DelangheJorritZombieRuntime/BTTask_PickItemUp_DelangheJorrit.h
@@ -0,0 +1,20 @@
+﻿#pragma once
+#include"CoreMinimal.h"
+#include "BehaviorTree/BTTaskNode.h"
+#include "BTTask_PickItemUp_DelangheJorrit.generated.h"
+
+UCLASS()
+class DELANGHEJORRITZOMBIERUNTIME_API UBTTask_PickItemUp_DelangheJorrit final: public UBTTaskNode
+{
+	GENERATED_BODY()
+public:
+	explicit UBTTask_PickItemUp_DelangheJorrit();
+	
+	//called when Behavour tree reaches node, one time
+	virtual EBTNodeResult::Type ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) override;
+	
+private:
+	UPROPERTY(EditAnywhere,Category="Blackboard")
+	FBlackboardKeySelector TargetItemKey{};
+	
+};
