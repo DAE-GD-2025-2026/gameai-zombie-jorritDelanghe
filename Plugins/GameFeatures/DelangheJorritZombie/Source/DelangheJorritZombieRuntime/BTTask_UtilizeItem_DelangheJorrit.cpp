6e213aadc0e2e62b0c2f7a23b2b02cb090881647
jorritDelanghe <157893404+jorritDelanghe@users.noreply.github.com>
2026-05-31 08:15:32 +0200

    Use Medkit and food

---
+++ Plugins/GameFeatures/DelangheJorritZombie/Source/DelangheJorritZombieRuntime/BTTask_UtilizeItem_DelangheJorrit.cpp
@@ -0,0 +1,45 @@
+﻿#include "BTTask_UtilizeItem_DelangheJorrit.h"
+
+#include "AIController.h"
+#include "Common/InventoryComponent.h"
+#include "Items/Medkit.h"
+
+UBTTask_UtilizeItem_DelangheJorrit::UBTTask_UtilizeItem_DelangheJorrit()
+{
+	NodeName = "Use Items in inventory";
+}
+
+EBTNodeResult::Type UBTTask_UtilizeItem_DelangheJorrit::ExecuteTask(UBehaviorTreeComponent& OwnerComp,
+	uint8* NodeMemory)
+{
+	APawn* Pawn {OwnerComp.GetAIOwner()->GetPawn()};
+	if (!Pawn) return EBTNodeResult::Failed;
+	
+	constexpr int MinCapacity{1};
+	auto* Inventory{Pawn->GetComponentByClass<UInventoryComponent>()};
+	if (!Inventory) return EBTNodeResult::Failed;
+	if (Inventory->GetInventoryCapacity()<MinCapacity) return EBTNodeResult::Failed;
+	
+	auto* HealthComp{Pawn->GetComponentByClass<UHealthComponent>()};
+	auto* StaminaComp{Pawn->GetComponentByClass<UStaminaComponent>()};
+	
+	constexpr int MinHealthForHeal{6};
+	constexpr int MinStaminaForBoost{6};
+	for (int i{};i<Inventory->GetInventoryCapacity();++i)
+	{
+		const ABaseItem* item {Inventory->GetInventory()[i]};
+		if (!item) return EBTNodeResult::Failed;
+		
+		if (item->GetItemType() == EItemType::Medkit && HealthComp->GetHealth()<MinHealthForHeal)
+		{
+			Inventory->UseItem(i);
+			return EBTNodeResult::Succeeded;
+		}
+		if (item->GetItemType() == EItemType::Food && StaminaComp->GetCurrentStamina()<MinStaminaForBoost)
+		{
+			Inventory->UseItem(i);
+			return EBTNodeResult::Succeeded;
+		}
+	}
+	return Super::ExecuteTask(OwnerComp, NodeMemory);
+}
---
+++ Plugins/GameFeatures/DelangheJorritZombie/Source/DelangheJorritZombieRuntime/BTTask_UtilizeItem_DelangheJorrit.h
@@ -0,0 +1,12 @@
+﻿#pragma once
+#include "BehaviorTree/BTTaskNode.h"
+#include "CoreMinimal.h"
+#include "BTTask_UtilizeItem_DelangheJorrit.generated.h"
+UCLASS()
+class DELANGHEJORRITZOMBIERUNTIME_API UBTTask_UtilizeItem_DelangheJorrit final: public UBTTaskNode
+{
+	GENERATED_BODY()
+public:
+	explicit UBTTask_UtilizeItem_DelangheJorrit();
+	virtual EBTNodeResult::Type ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) override;
+};
