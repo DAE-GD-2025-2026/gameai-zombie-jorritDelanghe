882e95a647f7ddb3b1fe9d6f2b9e729b31ac66be
jorritDelanghe <157893404+jorritDelanghe@users.noreply.github.com>
2026-06-05 21:44:35 +0200

    filter so no garbage is collected

--- Plugins/GameFeatures/DelangheJorritZombie/Source/DelangheJorritZombieRuntime/BTTask_SetTargetItem_DelangheJorrit.cpp
+++ Plugins/GameFeatures/DelangheJorritZombie/Source/DelangheJorritZombieRuntime/BTTask_SetTargetItem_DelangheJorrit.cpp
@@ -21,8 +21,19 @@ EBTNodeResult::Type UBTTask_SetTargetItem_DelangheJorrit::ExecuteTask(UBehaviorT
 	const auto& Items{Perceptor->GetObservedItems()};
 	if (Items.IsEmpty()) return EBTNodeResult::Failed;
 	
+	//filter trough only the useful items, no garbage
+	TArray <ABaseItem*> UsefulItems;
+	for (auto Item : Items)
+	{
+		if (Item->GetItemType() != EItemType::Garbage)
+		{
+			UsefulItems.Add(Item);
+		}
+	}
+	if (UsefulItems.IsEmpty()) return EBTNodeResult::Failed;
+	
 	//look for nearest Item
-	ABaseItem* ClosestItem{FindClosestActor(Items,Pawn->GetActorLocation())};
+	ABaseItem* ClosestItem{FindClosestActor(UsefulItems,Pawn->GetActorLocation())};
 	
 	OwnerComp.GetBlackboardComponent()->SetValueAsObject(TargetItemKey.SelectedKeyName,ClosestItem);
 	return EBTNodeResult::Succeeded;
