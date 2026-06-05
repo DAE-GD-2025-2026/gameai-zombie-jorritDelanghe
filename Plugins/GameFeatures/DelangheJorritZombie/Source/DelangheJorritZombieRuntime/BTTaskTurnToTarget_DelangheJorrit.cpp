df11111ce217f8b74f84c5fbeff9b75c82811903
jorritDelanghe <157893404+jorritDelanghe@users.noreply.github.com>
2026-06-05 14:54:54 +0200

    face target

---
+++ Plugins/GameFeatures/DelangheJorritZombie/Source/DelangheJorritZombieRuntime/BTTaskTurnToTarget_DelangheJorrit.cpp
@@ -0,0 +1,25 @@
+﻿#include "BTTaskTurnToTarget_DelangheJorrit.h"
+#include "AIController.h"
+#include "BehaviorTree/BlackboardComponent.h"
+
+UBTTaskTurnToTarget_DelangheJorrit::UBTTaskTurnToTarget_DelangheJorrit()
+{
+	NodeName = "Turn to Target";
+}
+
+EBTNodeResult::Type UBTTaskTurnToTarget_DelangheJorrit::ExecuteTask(UBehaviorTreeComponent& OwnerComp,
+	uint8* NodeMemory)
+{
+	APawn* Pawn = OwnerComp.GetAIOwner()->GetPawn();
+	if (!Pawn) return EBTNodeResult::Failed;
+	
+	//read the target from the blackBoardkey
+	UBlackboardComponent* blackBoardComponent {OwnerComp.GetBlackboardComponent()};
+	AActor* Target{Cast<AActor>(blackBoardComponent->GetValueAsObject(GetSelectedBlackboardKey()))};
+	if (!Target) return EBTNodeResult::Failed;
+	
+	const FVector Direction = (Target->GetActorLocation() - Pawn->GetActorLocation()).GetSafeNormal();
+	Pawn->SetActorRotation(Direction.ToOrientationRotator());
+	
+	return EBTNodeResult::Succeeded;
+}
---
+++ Plugins/GameFeatures/DelangheJorritZombie/Source/DelangheJorritZombieRuntime/BTTaskTurnToTarget_DelangheJorrit.h
@@ -0,0 +1,13 @@
+﻿#pragma once
+#include "BehaviorTree/Tasks/BTTask_BlackboardBase.h"
+#include "BTTaskTurnToTarget_DelangheJorrit.generated.h"
+
+UCLASS()
+class DELANGHEJORRITZOMBIERUNTIME_API UBTTaskTurnToTarget_DelangheJorrit final : public UBTTask_BlackboardBase  
+{
+	GENERATED_BODY()
+public:
+	explicit UBTTaskTurnToTarget_DelangheJorrit();
+	virtual EBTNodeResult::Type ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) override;
+};
+
