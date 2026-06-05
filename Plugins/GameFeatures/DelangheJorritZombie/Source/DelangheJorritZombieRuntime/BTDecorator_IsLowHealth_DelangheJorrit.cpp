163f9b9e96c00ba9adceb54e1d98faf0784b1c85
jorritDelanghe <157893404+jorritDelanghe@users.noreply.github.com>
2026-06-05 21:38:33 +0200

    low health decorator

---
+++ Plugins/GameFeatures/DelangheJorritZombie/Source/DelangheJorritZombieRuntime/BTDecorator_IsLowHealth_DelangheJorrit.cpp
@@ -0,0 +1,20 @@
+﻿#include "BTDecorator_IsLowHealth_DelangheJorrit.h"
+#include "AIController.h"
+#include "StudentPerceptor_DelangheJorrit.h"
+
+UBTDecorator_IsLowHealth_DelangheJorrit::UBTDecorator_IsLowHealth_DelangheJorrit()
+{
+	NodeName = "Is low health" ;
+}
+
+bool UBTDecorator_IsLowHealth_DelangheJorrit::CalculateRawConditionValue(UBehaviorTreeComponent& OwnerComp,
+																					uint8* NodeMemory) const
+{
+	APawn* Pawn {OwnerComp.GetAIOwner()->GetPawn()};
+	if (!Pawn) return false;
+	
+	auto* Health{Pawn->GetComponentByClass<UHealthComponent>()};
+	if (!Health) return false;
+	
+	return Health->GetHealth()>= MinHealth;
+}
---
+++ Plugins/GameFeatures/DelangheJorritZombie/Source/DelangheJorritZombieRuntime/BTDecorator_IsLowHealth_DelangheJorrit.h
@@ -0,0 +1,15 @@
+﻿#pragma once
+#include "BehaviorTree/BTDecorator.h"
+#include "BTDecorator_IsLowHealth_DelangheJorrit.generated.h"
+
+UCLASS()
+class UBTDecorator_IsLowHealth_DelangheJorrit final : public UBTDecorator
+{
+	GENERATED_BODY()
+public:
+	UPROPERTY(EditAnywhere)
+	int MinHealth{3};
+	
+	explicit UBTDecorator_IsLowHealth_DelangheJorrit();
+	virtual bool CalculateRawConditionValue(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) const override;
+};
