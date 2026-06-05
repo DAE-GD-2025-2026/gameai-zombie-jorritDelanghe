a883e2ec55043e88c2320f6b008f373263b1a2e9
jorritDelanghe <157893404+jorritDelanghe@users.noreply.github.com>
2026-06-05 15:49:02 +0200

    task set zombie as target

---
+++ Plugins/GameFeatures/DelangheJorritZombie/Source/DelangheJorritZombieRuntime/BTTask_SetZombieAsTarget_DelangheJorrit.cpp
@@ -0,0 +1,29 @@
+﻿#include "BTTask_SetZombieAsTarget_DelangheJorrit.h"
+#include "StudentPerceptor_DelangheJorrit.h"
+#include "BehaviorTree/BlackboardComponent.h"
+#include "AIController.h"
+#include "BTHelpers_DelangheJorrit.h"
+
+UBTTask_SetZombieAsTarget_DelangheJorrit::UBTTask_SetZombieAsTarget_DelangheJorrit()
+{
+	NodeName = "Set Target Zombie";
+}
+
+EBTNodeResult::Type UBTTask_SetZombieAsTarget_DelangheJorrit::ExecuteTask(UBehaviorTreeComponent& OwnerComp,
+																	  uint8* NodeMemory)
+{
+	APawn* Pawn = OwnerComp.GetAIOwner()->GetPawn();
+	if (!Pawn) return EBTNodeResult::Failed;
+	
+	auto* Perceptor = Pawn->GetComponentByClass<UStudentPerceptor_DelangheJorrit>();
+	if (!Perceptor) return EBTNodeResult::Failed;
+	
+	const auto& Zombies{Perceptor->GetObservedZombies()};
+	if (Zombies.IsEmpty()) return EBTNodeResult::Failed;
+	
+	//look for nearest Item
+	ABaseZombie* ClosestZombie{FindClosestActor(Zombies,Pawn->GetActorLocation())};
+	
+	OwnerComp.GetBlackboardComponent()->SetValueAsObject(TargetZombieKey.SelectedKeyName,ClosestZombie);
+	return EBTNodeResult::Succeeded;
+}
---
+++ Plugins/GameFeatures/DelangheJorritZombie/Source/DelangheJorritZombieRuntime/BTTask_SetZombieAsTarget_DelangheJorrit.h
@@ -0,0 +1,19 @@
+﻿
+#pragma once
+#include "BehaviorTree/BTTaskNode.h"
+#include "BTTask_SetZombieAsTarget_DelangheJorrit.generated.h"
+UCLASS()
+class DELANGHEJORRITZOMBIERUNTIME_API UBTTask_SetZombieAsTarget_DelangheJorrit final: public UBTTaskNode
+{
+	GENERATED_BODY()
+public:
+	explicit UBTTask_SetZombieAsTarget_DelangheJorrit();
+	
+	//called when Behavour tree reaches node, one time
+	virtual EBTNodeResult::Type ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) override;
+	
+private:
+	UPROPERTY(EditAnywhere,Category="Blackboard")
+	FBlackboardKeySelector TargetZombieKey{};
+	
+};
