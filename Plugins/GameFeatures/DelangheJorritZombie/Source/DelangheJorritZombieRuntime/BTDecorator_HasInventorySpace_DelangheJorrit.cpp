0382c09e14c6eed7477fe886413081cb44780566
jorritDelanghe <157893404+jorritDelanghe@users.noreply.github.com>
2026-06-06 18:46:25 +0200

    Has inventory deco

---
+++ Plugins/GameFeatures/DelangheJorritZombie/Source/DelangheJorritZombieRuntime/BTDecorator_HasInventorySpace_DelangheJorrit.cpp
@@ -0,0 +1,27 @@
+﻿#include "BTDecorator_HasInventorySpace_DelangheJorrit.h"
+#include "AIController.h"
+#include "Common/InventoryComponent.h"
+
+UBTDecorator_HasInventorySpace_DelangheJorrit::UBTDecorator_HasInventorySpace_DelangheJorrit()
+{
+	NodeName = "Has Inventory Space";
+}
+
+bool UBTDecorator_HasInventorySpace_DelangheJorrit::CalculateRawConditionValue(UBehaviorTreeComponent& OwnerComp,
+	uint8* NodeMemory) const
+{
+	APawn* Pawn{OwnerComp.GetAIOwner()->GetPawn()};
+	if (!Pawn) return false;
+
+	auto* Inventory{Pawn->GetComponentByClass<UInventoryComponent>()};
+	if (!Inventory) return false;
+
+	for (int i{}; i < Inventory->GetInventoryCapacity(); ++i)
+	{
+		if (!Inventory->GetInventory()[i])
+			return true; // found empty slot
+	}
+	return false;
+}
+
+
