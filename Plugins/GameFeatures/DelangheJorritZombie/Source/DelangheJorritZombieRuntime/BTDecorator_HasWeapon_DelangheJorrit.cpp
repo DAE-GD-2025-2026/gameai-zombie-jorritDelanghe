31e639954665c29b91eef1a4973ed87c48edfc0c
jorritDelanghe <157893404+jorritDelanghe@users.noreply.github.com>
2026-06-07 18:45:44 +0200

    has weapon deco new

---
+++ Plugins/GameFeatures/DelangheJorritZombie/Source/DelangheJorritZombieRuntime/BTDecorator_HasWeapon_DelangheJorrit.cpp
@@ -0,0 +1,32 @@
+﻿#include "BTDecorator_HasWeapon_DelangheJorrit.h"
+#include "AIController.h"
+#include  "Common/InventoryComponent.h"
+
+
+UBTDecorator_HasWeapon_DelangheJorrit::UBTDecorator_HasWeapon_DelangheJorrit()
+{
+	NodeName = "Has Weapon";
+}
+
+bool UBTDecorator_HasWeapon_DelangheJorrit::CalculateRawConditionValue(
+	UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) const
+{
+	APawn* Pawn{OwnerComp.GetAIOwner()->GetPawn()};
+	if (!Pawn) return false;
+
+	auto* Inventory{Pawn->GetComponentByClass<UInventoryComponent>()};
+	if (!Inventory) return false;
+
+	for (int i{}; i < Inventory->GetInventoryCapacity(); ++i)
+	{
+		//null check
+		auto* Item = Inventory->GetInventory()[i];
+		if (!Item) continue;
+		
+		//look for gun
+		if (Inventory->GetInventory()[i]->GetItemType() == EItemType::Pistol
+			|| Inventory->GetInventory()[i]->GetItemType() == EItemType::Shotgun)
+			return true; 
+	}
+	return false;
+}
---
+++ Plugins/GameFeatures/DelangheJorritZombie/Source/DelangheJorritZombieRuntime/BTDecorator_HasWeapon_DelangheJorrit.h
@@ -0,0 +1,12 @@
+﻿#pragma once
+#include "BehaviorTree/BTDecorator.h"
+#include "BTDecorator_HasWeapon_DelangheJorrit.generated.h"
+
+UCLASS()
+class DELANGHEJORRITZOMBIERUNTIME_API UBTDecorator_HasWeapon_DelangheJorrit final : public UBTDecorator
+{
+	GENERATED_BODY()
+public:	
+	explicit UBTDecorator_HasWeapon_DelangheJorrit();
+	virtual bool CalculateRawConditionValue(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) const override;
+};
