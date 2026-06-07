a5d722d8a60ec8e548de90e3b966aa3d5326334b
jorritDelanghe <157893404+jorritDelanghe@users.noreply.github.com>
2026-06-07 18:45:36 +0200

    Has inventory restored deco

---
+++ Plugins/GameFeatures/DelangheJorritZombie/Source/DelangheJorritZombieRuntime/BTDecorator_HasInventorySpace_DelangheJorrit.h
@@ -0,0 +1,12 @@
+﻿#pragma once
+#include "BehaviorTree/BTDecorator.h"
+#include "BTDecorator_HasInventorySpace_DelangheJorrit.generated.h"
+
+UCLASS()
+class DELANGHEJORRITZOMBIERUNTIME_API UBTDecorator_HasInventorySpace_DelangheJorrit final : public UBTDecorator
+{
+	GENERATED_BODY()
+public:	
+	explicit UBTDecorator_HasInventorySpace_DelangheJorrit();
+	virtual bool CalculateRawConditionValue(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) const override;
+};
